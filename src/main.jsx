45842dee92491b81e9dcc678551f39d770ca2f35
duc098182 <lienminh0149@gmail.com>
2024-11-28 14:33:09 +0700

    Update UI for header, footer and add About page

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import App from './App.jsx'
 import LoginPage from './pages/login.jsx'
 import RegisterPage from './pages/register.jsx'
 import UserPage from './pages/user.jsx'
+
+import AboutPage from './pages/about.jsx'
+
 import ReactDOM from 'react-dom/client'
 import ErrorPage from './pages/error.jsx'
 import { AuthWrapper } from './components/context/auth_context.jsx'
@@ -23,6 +26,10 @@ const router = createBrowserRouter([
       {
         path: "/users",
         element: <UserPage />,
+      },
+      {
+        path: "/about",
+        element: <AboutPage />,        
       },
     ]
   },
